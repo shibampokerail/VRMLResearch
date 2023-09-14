29dce3745816fc4f87f241296ecf6fea80072e72
Owen Murphy <owentmurp@gmail.com>
2023-09-14 13:02:04 -0700

    Adding computer skill field

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
+from datas import AGE_GROUPS, ALL_SUCCESS_DURATION, ALL_GENDER
+
 
 # Assuming X1, X2, and y are your feature and label arrays
 X = np.column_stack((X1, X2))
